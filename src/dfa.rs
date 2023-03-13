5f5c3915e47ce39c4657e8b98ee6b54a4f60a09a
Lef Ioannidis <elefthei@seas.upenn.edu>
2023-03-13 13:56:09 -0400

    WIP

--- src/dfa.rs
+++ src/dfa.rs
@@ -7,29 +7,22 @@ use crate::deriv::{deriv, nullable};
 use crate::parser::re::Regex;
 
 #[derive(Debug)]
-pub struct DFA<'a> {
+pub struct DFA {
     /// Alphabet
-    pub ab: &'a str,
-    /// map of alphabet -> u64
-    pub chars: HashMap<char, u64>,
+    pub ab: Vec<String>,
     /// Set of states (and their index)
     pub states: HashMap<Regex, u64>,
-    /// Transition relation from [state -> state], given [char]
-    pub trans: HashSet<(Regex, char, Regex)>,
+    /// Transition relation from [state -> state] given an input
+    pub trans: HashMap<(Regex, String), Regex>,
 }
 
-impl<'a> DFA<'a> {
-    pub fn new(ab: &'a str, re: Regex) -> Self {
-        let mut char_map = HashMap::new();
-        for (i, c) in ab.chars().sorted().enumerate() {
-            char_map.insert(c, i as u64);
-        }
+impl DFA {
+    pub fn new<'a>(ab: &'a str, re: Regex) -> Self {
 
         let mut d = Self {
-            ab,
-            chars: char_map,
+            ab: ab.chars().sorted().map(|c| c.to_string()).collect(),
             states: HashMap::new(),
-            trans: HashSet::new(),
+            trans: HashMap::new(),
         };
 
         // Recursive funtion
@@ -38,9 +31,9 @@ impl<'a> DFA<'a> {
           d.add_state(q);
 
           // Explore derivatives
-          for c in d.ab.chars() {
-              let q_c = deriv(c, q);
-              d.add_transition(q, c, &q_c);
+          for c in d.ab.clone().into_iter() {
+              let q_c = deriv(&c, q);
+              d.add_transition(q, &c, &q_c);
               if d.contains_state(&q_c) {
                   continue;
               } else {
@@ -54,14 +47,15 @@ impl<'a> DFA<'a> {
         d
     }
 
-    pub fn ab_to_num(&self, c: char) -> u64 {
+    pub fn ab_to_num(&self, c: &String) -> u64 {
         /*let sorted_ab = self.ab.chars().sorted().collect::<String>();
         let num = sorted_ab.chars().position(|x| x == c).unwrap();
         num as u64
         */
-        match c {
-            '#' => self.chars.len() as u64, // TODO better solution
-            _ => self.chars[&c],
+        if c == "" {
+            self.ab.len() as u64 // TODO better solution
+        } else {
+            self.ab.iter().position(|x| x == c).unwrap() as u64
         }
     }
 
@@ -69,8 +63,8 @@ impl<'a> DFA<'a> {
         self.states.len()
     }
 
-    pub fn add_transition(&mut self, from: &Regex, c: char, to: &Regex) {
-        self.trans.insert((from.clone(), c, to.clone()));
+    pub fn add_transition(&mut self, from: &Regex, c: &String, to: &Regex) {
+        self.trans.insert((from.clone(), c.clone()), to.clone());
     }
 
     pub fn add_state(&mut self, new_state: &Regex) {
@@ -83,6 +77,12 @@ impl<'a> DFA<'a> {
 
     pub fn get_state_num(&self, state: &Regex) -> u64 {
         self.states[state]
+    }
+
+    pub fn get_state_regex(&self, n: &u64) -> Option<Regex> {
+        self.states
+            .iter()
+            .find_map(|(&k, v)| if v == n { Some(k) } else { None })
     }
 
     /// Initial state
@@ -114,46 +114,60 @@ impl<'a> DFA<'a> {
     }
 
     /// DFA step function [delta(s, c) = s'] function
-    pub fn delta(&self, state: u64, ch: char) -> Result<u64> {
-        let res: Vec<u64> = self
-            .deltas()
-            .clone()
-            .into_iter()
-            .filter_map(|(s, c, t)| if s == state && c == ch { Some(t) } else { None })
-            .collect();
-
-        if res.len() == 1 {
-            Ok(res[0])
-        } else {
-            Err(Error::new(
-                ErrorKind::InvalidInput,
-                "Invalidated DFA invariant (determinism)",
-            ))
-        }
+    pub fn delta(&self, state: &u64, ch: &String) -> Result<u64> {
+        self.get_state_regex(&state)
+            .and_then(|r| self.trans.get(&(r, *ch)))
+            .map(|s| self.get_state_num(s))
+            .ok_or(Error::new(ErrorKind::InvalidInput,
+                    "Invalidated DFA invariant (determinism)"))
     }
 
-    pub fn deltas(&self) -> Vec<(u64, char, u64)> {
+    pub fn deltas(&self) -> Vec<(u64, String, u64)> {
         self.trans
             .clone()
             .into_iter()
-            .map(|(a, b, c)| (self.get_state_num(&a), b, self.get_state_num(&c)))
+            .map(|((a, b), c)| (self.get_state_num(&a), b, self.get_state_num(&c)))
             .collect()
     }
 
-    pub fn is_match(&self, doc: &String) -> bool {
+    pub fn is_match(&self, doc: &Vec<String>) -> bool {
         let mut s = self.get_init_state();
-        for c in doc.chars() {
-            s = self.delta(s, c).unwrap();
+        for c in doc.iter() {
+            s = self.delta(&s, c).unwrap();
         }
         // If it is in the final states, then success
         self.get_final_states().contains(&s)
     }
 
-    pub fn equiv_classes(&self) -> HashMap<char, HashSet<char>> {
-        let mut char_classes: HashMap<char, HashSet<char>> = HashMap::new();
+    /// Double the stride of the DFA, can be nested k-times
+    /// TODO: Figure out accepting states
+    ///       Figure out O(|ab|*n^2) algorithm
+    pub fn double_stride(&mut self) {
+        let mut ab = Vec::new();
+        let mut ntrans = HashMap::new();
+        for c0 in self.ab {
+            for c1 in self.ab {
+                for a in self.states.keys() {
+                    let b = self.trans.get(&(*a, c0)).unwrap();
+                    let c = self.trans.get(&(*b, c1)).unwrap();
+                    ntrans.insert((*a, c0 + &c1), *c);
+                    ab.push(c0 + &c1);
+                }
+            }
+        }
 
-        for a in self.ab.chars() {
-            for b in self.ab.chars() {
+        self.trans = ntrans;
+        self.ab = ab;
+    }
+
+    /// Compute equivalence classes from the DFA
+    /// and output for each character its equivalence class
+    /// TODO: Make algorithm O(|ab|*n)
+    pub fn equiv_classes(&self) -> HashMap<String, HashSet<String>> {
+        let mut char_classes: HashMap<String, HashSet<String>> = HashMap::new();
+
+        for a in self.ab {
+            for b in self.ab {
                 if !char_classes.contains_key(&a) {
                     char_classes.insert(a, HashSet::from([a]));
                 }
@@ -162,13 +176,13 @@ impl<'a> DFA<'a> {
                 }
                 let mut equivalent = true;
                 for s in self.get_states() {
-                    if self.delta(s, a).unwrap() != self.delta(s, b).unwrap() {
+                    if self.delta(&s, &a).unwrap() != self.delta(&s, &b).unwrap() {
                         equivalent = false;
                     }
                 }
                 // Merge equivalence classes
                 if equivalent {
-                    let union: HashSet<char> =
+                    let union: HashSet<String> =
                         char_classes[&a].union(&char_classes[&b]).cloned().collect();
                     char_classes.insert(a, union.clone());
                     char_classes.insert(b, union);
@@ -189,13 +203,13 @@ mod tests {
     fn set_up_delta_test(r: &str, alpha: &str, tocheck: &str) -> bool {
         let ab = String::from(alpha);
         let regex = regex_parser(&String::from(r), &ab);
-        let input = String::from(tocheck);
+        let input : Vec<String> = tocheck.chars().map(|c| c.to_string()).collect();
 
         let mut dfa = DFA::new(&ab[..], regex);
         let mut s = dfa.get_init_state();
 
         for i in 0..input.len() {
-            s = dfa.delta(s, input.chars().nth(i).unwrap()).unwrap();
+            s = dfa.delta(&s, &input[i]).unwrap();
         }
         let re_match = dfa.get_final_states().contains(&s);
         return re_match;
