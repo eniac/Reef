11ac97f1e88776d485ce8d04aef7e02321abbb19
jkwoods <woodsjk@seas.upenn.edu>
2023-04-12 17:21:18 -0400

    oops, upper bound doc mle correctly

--- src/backend/framework.rs
+++ src/backend/framework.rs
@@ -87,7 +87,7 @@ pub fn gen_commitment(
             let doc_ext_len = doc.len().next_power_of_two();
 
             let mut doc_ext: Vec<Integer> = doc.into_iter().map(|x| Integer::from(x)).collect();
-            doc_ext.append(&mut vec![Integer::from(0); doc_ext_len]);
+            doc_ext.append(&mut vec![Integer::from(0); doc_ext_len - doc_ext.len()]);
 
             let mut mle = mle_from_pts(doc_ext);
             println!("mle: {:#?}", mle);
