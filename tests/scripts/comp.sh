078e56152f7ebf654540d06a1a0e060904abae56
Eli Margolin <ecmargolin@gmail.com>
2023-10-17 22:40:55 -0400

    rebase

--- tests/scripts/comp.sh
+++ tests/scripts/comp.sh
@@ -1,12 +1,12 @@
-# cargo clean 
-# cargo build --release --features 'metrics,reef'
+cargo clean 
+cargo build --release --features 'metrics,reef'
 #echo "reef"
 #./tests/scripts/reef.sh &> out_reef
 #echo "end reef"
 
-# echo "reef h"
-# ./tests/scripts/reef_h.sh &> out_rh 
-# echo "reef h"
+echo "reef h"
+./tests/scripts/reef_h.sh &> out_rh 
+echo "reef h"
 
 # cargo build --release --features 'metrics,nwr'
 # echo "nwr"
@@ -33,9 +33,9 @@
 # ./tests/scripts/dna.sh &> out_dna
 # echo "dna"
 
-echo "pihole"
-./tests/scripts/pihole.sh &> out_pihole 
-echo "pihole" 
+# echo "pihole"
+# ./tests/scripts/pihole.sh &> out_pihole 
+# echo "pihole" 
 
 echo "email naive for constraints"
 RUST_BACKTRACE=1 ./target/release/reef --input ./tests/docs/email_med --output ./tests/results/email_dkim_naive --re "^Message-ID: .*[[:space:]]Date: Tue, 11 Jul 2000 11:11:00 -0700 \(PDT\)[[:space:]]From: .*[[:space:]]To: .*[[:space:]]Subject: Reimbursement of Individually Billed Items[[:space:]]Mime-Version: 1\.0[[:space:]]Content-Type: text\/plain; charset=us-ascii[[:space:]]Content-Transfer-Encoding: 7bit[[:space:]]X-From: Enron Announcements[[:space:]]X-To: All Enron Employees North America[[:space:]]X-cc: [[:space:]]X-bcc: [[:space:]]X-Folder: \\\\Michelle_Lokay_Dec2000_June2001_1\\\\Notes Folders\\\\Corporate[[:space:]]X-Origin: LOKAY-M[[:space:]]X-FileName: mlokay\.nsf[[:space:]]*The memo distributed on June 27 on Reimbursement of Individually Billed Items [[:space:]]requires[[:space:]]clarification\.  The intent of the memo was to give employees an alternate [[:space:]]method[[:space:]]of paying for pagers, cell phones, etc\.  Employees can continue to submit [[:space:]]these[[:space:]]invoices to Accounts Payable for processing or  pay these items with their [[:space:]]corporate[[:space:]]American Express card and request reimbursement through an expense report\.  [[:space:]]Either[[:space:]]way is an acceptable way to process these small dollar high volume invoices\.$" ascii > out_email_naive
