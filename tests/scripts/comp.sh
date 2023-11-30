7655eff2c1eef37e51226c52ca55bbe8d6bef3e4
Eli <ecmargolin@gmail.com>
2023-11-30 13:43:52 -0500

    minor formatting

--- tests/scripts/comp.sh
+++ tests/scripts/comp.sh
@@ -4,10 +4,6 @@ cargo clean
 # for i in {1..10}; do ./tests/scripts/reef_h.sh &> out_rh; done
 # echo "reef h"
 
-echo "reef"
-./tests/scripts/reef.sh &> out_reef
-echo "end reef"
-
 cargo clean 
 cargo build --release --features 'metrics,nwr'
 echo "nwr"
@@ -19,6 +15,10 @@ cargo build --release --features 'metrics,naive'
 echo "naive"
 ./tests/scripts/naive.sh &> out_naive
 echo "naive" 
+
+echo "reef"
+./tests/scripts/reef.sh &> out_reef
+echo "end reef"
 
 
 
