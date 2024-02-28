f4e5913dd117172d337900f22e5b94aadb6339ac
Eli <ecmargolin@gmail.com>
2024-02-28 14:55:57 -0500

    naive

--- tests/scripts/full_comp.sh
+++ tests/scripts/full_comp.sh
@@ -7,14 +7,14 @@
 # ./tests/scripts/safa_nlookup.sh &> out_safa_nlookup
 # echo "end safa+nlookup"
 
-cargo clean 
-cargo build --release --features 'metrics,nwr'
-echo "nwr"
-./tests/scripts/nwr.sh &> out_nwr
-echo "end nwr"
+# cargo clean 
+# cargo build --release --features 'metrics,nwr'
+# echo "nwr"
+# ./tests/scripts/nwr.sh &> out_nwr
+# echo "end nwr"
 
-cargo clean 
-cargo build --release --features 'metrics,naive'
+# cargo clean 
+# cargo build --release --features 'metrics,naive'
 echo "naive"
 ./tests/scripts/naive.sh &> out_naive
 echo "end naive" 
