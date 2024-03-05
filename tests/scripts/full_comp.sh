2e08ee0ca4fb87493538ef1173be28c638be8891
Eli <ecmargolin@gmail.com>
2024-03-05 08:27:16 -0500

    mem

--- tests/scripts/full_comp.sh
+++ tests/scripts/full_comp.sh
@@ -1,11 +1,11 @@
 # cargo clean 
 # cargo build --release --features 'metrics'
-# echo "reef"
-# ./tests/scripts/reef.sh &> out_reef
+echo "reef"
+./tests/scripts/reef.sh &> out_reef
 
-# echo "safa+nlookup"
-# ./tests/scripts/safa_nlookup.sh &> out_safa_nlookup
-# echo "end safa+nlookup"
+echo "safa+nlookup"
+./tests/scripts/safa_nlookup.sh &> out_safa_nlookup
+echo "end safa+nlookup"
 
 # cargo clean 
 # cargo build --release --features 'metrics,nwr'
@@ -15,6 +15,6 @@
 
 # cargo clean 
 # cargo build --release --features 'metrics,naive'
-echo "naive"
-./tests/scripts/naive.sh &> out_naive
-echo "end naive" 
+# echo "naive"
+# ./tests/scripts/naive.sh &> out_naive
+# echo "end naive" 
